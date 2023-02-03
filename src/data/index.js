ae0e10da438967d0d40b0943f61f268a7462fe50
LeginchukYurij <leginchuk.yurij@gmail.com>
2023-02-03 22:01:51 +0200

    Added hero slider component with fake data

---
+++ src/data/index.js
@@ -0,0 +1,19 @@
+import slideImage from '../assets/main-slider.jpg';
+
+export const heroSlides = [
+  {
+    title: 'Ваша ІДЕАЛЬНА ванна кімната',
+    img: slideImage,
+    href: '/',
+  },
+  {
+    title: 'Ваша ІДЕАЛЬНА ванна кімната',
+    img: slideImage,
+    href: '/',
+  },
+  {
+    title: 'Ваша ІДЕАЛЬНА ванна кімната',
+    img: slideImage,
+    href: '/',
+  },
+];
